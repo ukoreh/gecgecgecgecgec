c6eda8b88067850fd5667bc23df20228efaa9623
freitzzz <1160907@isep.ipp.pt>
2023-05-16 22:41:21 +0100

    feat: add workflow job status stepper to main page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,55 @@
-<script>
-	import { DropInput } from '@components';
+<script lang="ts">
+	import { DropInput, WorkflowJobStatusStepper } from '@components';
+	import type { WorkflowJobStep } from '@models';
+
+	const steps = new Array<WorkflowJobStep>(
+		...[
+			{
+				name: 'Set up job',
+				status: 'completed',
+				conclusion: 'success',
+				number: 1,
+				started_at: '2023-05-15T20:15:18.000Z',
+				completed_at: '2023-05-15T20:15:21.000Z'
+			},
+			{
+				name: 'Clone or Checkout repository',
+				status: 'completed',
+				conclusion: 'success',
+				number: 2,
+				started_at: '2023-05-15T20:15:21.000Z',
+				completed_at: '2023-05-15T20:15:23.000Z'
+			},
+			{
+				name: 'Set up Flutter',
+				status: 'in_progress',
+				conclusion: 'success',
+				number: 3,
+				started_at: '2023-05-15T20:15:23.000Z',
+				completed_at: '2023-05-15T20:16:17.000Z'
+			},
+			{
+				name: 'Clean flutter',
+				status: 'queued',
+				conclusion: 'success',
+				number: 4,
+				started_at: '2023-05-15T20:16:17.000Z',
+				completed_at: '2023-05-15T20:16:22.000Z'
+			},
+			{
+				name: 'Get packages',
+				status: 'queued',
+				conclusion: 'success',
+				number: 5,
+				started_at: '2023-05-15T20:16:22.000Z',
+				completed_at: '2023-05-15T20:16:27.000Z'
+			}
+		]
+	);
 </script>
 
 <DropInput />
+
+<div class="flex justify-center">
+	<WorkflowJobStatusStepper {steps} />
+</div>
