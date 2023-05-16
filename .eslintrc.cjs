05826c4d058921fa33084dab7c2a7cbaef1d550f
freitzzz <1160907@isep.ipp.pt>
2023-05-16 20:52:38 +0100

    fix: add missing rules

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -12,7 +12,14 @@ module.exports = {
 		sourceType: 'module',
 		ecmaVersion: 2020
 	},
-	rules: {},
+	rules: {
+		'no-unused-vars': 'off',
+		'unused-imports/no-unused-imports': 'error',
+		'unused-imports/no-unused-vars': [
+			'warn',
+			{ vars: 'all', varsIgnorePattern: '^_', args: 'after-used', argsIgnorePattern: '^_' }
+		]
+	},
 	env: {
 		browser: true,
 		es2017: true,
