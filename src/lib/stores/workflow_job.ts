45e73639d56a2dff71a37049a5a4502dad276590
freitzzz <1160907@isep.ipp.pt>
2023-05-19 19:35:02 +0100

    feat: create store for workflow job updates

---
+++ src/lib/stores/workflow_job.ts
@@ -0,0 +1,37 @@
+import type { WorkflowJob, WorkflowRunId } from "@models";
+import { createStore, type Store } from "./store";
+import { FakeWorkflowsImpl, type Workflows } from "@http";
+import { State, from, type TypedState } from "./state";
+
+export const WorkflowJobStore = createWorkflowJobStore();
+
+function createWorkflowJobStore() {
+    const store = createStore<WorkflowJob>();
+    const subscribe = store.subscribe;
+
+    const workflows = new FakeWorkflowsImpl();
+
+    return {
+        subscribe,
+        observe: (id: WorkflowRunId) => observeJobState(id, workflows, store)
+    };
+}
+
+async function observeJobState(id: WorkflowRunId, workflows: Workflows, store: Store<WorkflowJob>) {
+    setInterval(
+        () => updateJobState(id, workflows, store),
+        3000
+    );
+}
+
+async function updateJobState(id: WorkflowRunId, workflows: Workflows, store: Store<WorkflowJob>) {
+    const job = await workflows.status(id);
+
+    if (job instanceof Response) {
+        return;
+    }
+
+    const state = from<WorkflowJob>(job, State.success);
+
+    store.set(state);
+}
