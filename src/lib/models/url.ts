d581e69e937aad6e516d24f3ef455d978f977408
freitzzz <1160907@isep.ipp.pt>
2023-05-21 18:32:36 +0100

    feat: describe error message when repo url could not be parsed

--- src/lib/models/url.ts
+++ src/lib/models/url.ts
@@ -5,7 +5,7 @@ export function createRepoUrl(url: string) {
 		const parsedUrl = new URL(url);
 
 		return parsedUrl.host === 'github.com' ? parsedUrl : undefined;
-	} catch (error) {
-		console.error(error);
+	} catch (error: unknown) {
+		console.warn(`could not parse ${url}: ${error}`);
 	}
 }
