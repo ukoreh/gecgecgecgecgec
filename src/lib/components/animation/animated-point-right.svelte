66aff3bfa97fce5b0518e3b90bba844c232843ac
freitzzz <1160907@isep.ipp.pt>
2023-05-21 14:57:29 +0100

    feat: add spacing after point right hand emoji

--- src/lib/components/animation/animated-point-right.svelte
+++ src/lib/components/animation/animated-point-right.svelte
@@ -1,5 +1,11 @@
 <script lang="ts">
 	import './index.css';
+
+	export let includeSelfSpace = true;
 </script>
 
 <span class="point-animation">👉</span>
+
+{#if includeSelfSpace}
+	<div class="w-4" />
+{/if}
