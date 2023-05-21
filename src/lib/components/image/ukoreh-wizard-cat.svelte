0a1352665ec2c5260f4ca502c4f61861050422cf
freitzzz <1160907@isep.ipp.pt>
2023-05-21 11:16:04 +0100

    refactor: update ukoreh wizard cat image

--- src/lib/components/image/ukoreh-wizard-cat.svelte
+++ src/lib/components/image/ukoreh-wizard-cat.svelte
@@ -2,4 +2,4 @@
 	import { base } from '$app/paths';
 </script>
 
-<img src="{base}/png/cat2.png" alt="wizard cat. it's a shame you can't see him! :(" />
+<img src="{base}/png/cat.png" alt="wizard cat. it's a shame you can't see him! :(" />
