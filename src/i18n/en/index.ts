6e41dbbd26336b8a9fe0d85cd1341c41ad447df2
freitzzz <1160907@isep.ipp.pt>
2023-05-21 18:39:26 +0100

    feat: localize workflow status divider label

--- src/i18n/en/index.ts
+++ src/i18n/en/index.ts
@@ -3,7 +3,8 @@ import type { BaseTranslation } from '../i18n-types';
 const en = {
 	title: '🪄 Ukoreh 🪄',
 	description: 'Drop any Flutter project link and see the magic happen!',
-	deploy: 'Deploy'
+	deploy: 'Deploy',
+	workflowDividerLabel: 'Workflow Status',
 } satisfies BaseTranslation;
 
 export default en;
--- src/i18n/pt/index.ts
+++ src/i18n/pt/index.ts
@@ -3,7 +3,8 @@ import type { Translation } from '../i18n-types';
 const pt = {
 	title: '🪄 Ukoreh 🪄',
 	description: 'Esbanja ai o link de um projeto de Flutter e vê o que acontece!',
-	deploy: 'Deploy'
+	deploy: 'Deploy',
+	workflowDividerLabel: 'Estado do Workflow',
 } satisfies Translation;
 
 export default pt;
