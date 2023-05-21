daae1f748c6300252866d0e604971293fde2c46a
freitzzz <1160907@isep.ipp.pt>
2023-05-21 18:41:03 +0100

    style: format

--- src/i18n/en/index.ts
+++ src/i18n/en/index.ts
@@ -4,7 +4,7 @@ const en = {
 	title: '🪄 Ukoreh 🪄',
 	description: 'Drop any Flutter project link and see the magic happen!',
 	deploy: 'Deploy',
-	workflowDividerLabel: 'Workflow Status',
+	workflowDividerLabel: 'Workflow Status'
 } satisfies BaseTranslation;
 
 export default en;
