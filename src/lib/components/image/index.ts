edebc6983b07c263496309058f4ed4d2fd108217
freitzzz <1160907@isep.ipp.pt>
2023-05-22 21:36:01 +0100

    feat: add sad and tired cat images

--- src/lib/components/image/index.ts
+++ src/lib/components/image/index.ts
@@ -1,1 +1,3 @@
 export { default as UkorehWizardCat } from './ukoreh-wizard-cat.svelte';
+export { default as TiredCat } from './tired-cat.svelte';
+export { default as SadCat } from './sad-cat.svelte';
---
+++ src/lib/components/image/sad-cat.svelte
@@ -0,0 +1,5 @@
+<script lang="ts">
+	import { base } from '$app/paths';
+</script>
+
+<img src="{base}/svg/sad_cat.svg" alt="sad cat. it's a shame you can't see him! :(" />
