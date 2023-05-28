d3e44720ccdaf895e6aec85c59a629a2aa19bfc4
freitzzz <1160907@isep.ipp.pt>
2023-05-28 12:56:35 +0100

    feat: get job logs when request fails and apply them to failure step

--- src/lib/stores/workflow_job.ts
+++ src/lib/stores/workflow_job.ts
@@ -7,7 +7,8 @@ import {
 	type WorkflowRunUrl,
 	unwrapLeft,
 	unwrapRight,
-	hasJobFailed
+	hasJobFailed,
+	hasFailed
 } from '@models';
 import { createStore, type Store } from './store';
 import { FoldClient, FolderWorkflowsImpl, type Workflows } from '@http';
@@ -15,10 +16,16 @@ import { State, from } from './state';
 
 export const WorkflowJobStore = createWorkflowJobStore();
 
-type WorkflowStateFailureReason = 'invalid-repo-url' | 'unknown-error';
+const logsPrefixRegex =
+	/\d{4}-[01]\d-[0-3]\dT[0-2]\d:[0-5]\d:[0-5]\d\.\d+([+-][0-2]\d:[0-5]\d|Z) /g;
+
+const endGroupSectionRegex = /##\[endgroup\]/g;
+const errorSectionRegex = /##\[error\]/g;
+
+type WorkflowStatestepFailureReason = 'invalid-repo-url' | 'unknown-error';
 
 type WorkflowStateFailure = {
-	reason: WorkflowStateFailureReason;
+	reason: WorkflowStatestepFailureReason;
 	message: string;
 };
 
@@ -48,7 +55,7 @@ async function triggerBuildJob(url: RepoUrl, workflows: Workflows, store: Store<
 	if (isLeft(init)) {
 		const value = unwrapLeft(init);
 
-		const reason: WorkflowStateFailureReason =
+		const reason: WorkflowStatestepFailureReason =
 			value.status === 400 ? 'invalid-repo-url' : 'unknown-error';
 
 		const state = from(
@@ -125,4 +132,44 @@ async function updateJobState(
 			state
 		)
 	);
+
+	if (state === State.failure) {
+		const logsTry = await workflows.logs(value.id);
+
+		if (isLeft(logsTry)) {
+			return;
+		}
+
+		const logs = unwrapRight(logsTry);
+
+		const failureLogs =
+			logs
+				.split(endGroupSectionRegex)
+				.findLast((x) => x)
+				?.split(errorSectionRegex)
+				?.find((x) => x)
+				?.replaceAll(logsPrefixRegex, '> ')
+				?.split('\n') ?? [];
+
+		store.update(function (x) {
+			const job = x.value;
+
+			job.steps = job.steps.map(
+				function (step) {
+					if (hasFailed(step)) {
+						step.logs = failureLogs;
+					}
+
+					return step;
+				}
+			);
+
+			return from(
+				<WorkflowState>{
+					...job
+				},
+				state
+			);
+		});
+	}
 }
