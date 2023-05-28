bffc49614967e8beea4ce0c4cc53b80492ba3dbe
freitzzz <1160907@isep.ipp.pt>
2023-05-28 19:18:16 +0100

    feat: add typewriter transition on drop input

--- src/lib/components/input/drop-input.svelte
+++ src/lib/components/input/drop-input.svelte
@@ -1,12 +1,29 @@
 <script lang="ts">
+	import { typewriter } from '../transition';
+
 	export let value: string;
 	export let disabled = false;
+	export let useTypewriterTransition: boolean = false;
+
+	function transition(input: HTMLInputElement) {
+		return typewriter(input, value);
+	}
 </script>
 
-<input
-	class="input input-block input-ghost-primary"
-	placeholder="https://github.com/freitas-labs/flutter-extended-image-crop-info-callback-spike"
-	type="text"
-	bind:value
-	{disabled}
-/>
+{#if useTypewriterTransition}
+	<input
+		class="input input-block input-ghost-primary"
+		placeholder="https://github.com/freitas-labs/flutter-extended-image-crop-info-callback-spike"
+		type="text"
+		in:transition
+		disabled={true}
+	/>
+{:else}
+	<input
+		class="input input-block input-ghost-primary"
+		placeholder="https://github.com/freitas-labs/flutter-extended-image-crop-info-callback-spike"
+		type="text"
+		bind:value
+		{disabled}
+	/>
+{/if}
